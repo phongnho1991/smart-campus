e5b6e0ac2ce1d503c4dd5bf103bfa00e7ebe8e53
shenke <1210541887@qq.com>
2019-07-03 15:22:59 +0800

    增加

--- sc-user-center/sc-user-center-server/src/main/java/net/zdsoft/smartcampus/user/config/ResourceServerConfig.java
+++ sc-user-center/sc-user-center-server/src/main/java/net/zdsoft/smartcampus/user/config/ResourceServerConfig.java
@@ -1,10 +1,16 @@
 package net.zdsoft.smartcampus.user.config;
 
+import org.springframework.boot.autoconfigure.security.oauth2.resource.JwtAccessTokenConverterRestTemplateCustomizer;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+import java.util.List;
 
 /**
  * @author shenke
@@ -26,12 +32,13 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                 .and().httpBasic();
     }
 
-    //@LoadBalanced
-    //@Bean
-    //public JwtAccessTokenConverterRestTemplateCustomizer loadBalanceJwtAccessTokenRestTemplateCustomer() {
-    //    return template -> {
-    //        template.
-    //    }
-    //}
+    @Bean
+    public JwtAccessTokenConverterRestTemplateCustomizer loadBalanceJwtAccessTokenRestTemplateCustomer(LoadBalancerInterceptor loadBalancerInterceptor) {
+        return template-> {
+            List<ClientHttpRequestInterceptor> interceptors = template.getInterceptors();
+            interceptors.add(loadBalancerInterceptor);
+            template.setInterceptors(interceptors);
+        };
+    }
 
 }

df687680c226b3df7950049c1d2d20a31736631e
shenke <1210541887@qq.com>
2019-07-03 15:23:16 +0800

    增加

--- sc-auth-center/src/main/java/net/zdsoft/smartcampus/auth/config/AuthorizationServerConfiguration.java
+++ sc-auth-center/src/main/java/net/zdsoft/smartcampus/auth/config/AuthorizationServerConfiguration.java
@@ -3,24 +3,26 @@ package net.zdsoft.smartcampus.auth.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.approval.ApprovalStore;
 import org.springframework.security.oauth2.provider.approval.ApprovalStoreUserApprovalHandler;
 import org.springframework.security.oauth2.provider.approval.JdbcApprovalStore;
 import org.springframework.security.oauth2.provider.approval.UserApprovalHandler;
-import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.rsa.crypto.KeyStoreKeyFactory;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.security.KeyPair;
 
 /**
  * @author shenke
@@ -54,7 +56,7 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
     }
 
     @Bean
-    public TokenStore tokenStore() {
+    public TokenStore tokenStore() throws IOException {
         JwtTokenStore jwtTokenStore = new JwtTokenStore(jwtAccessTokenConverter());
         jwtTokenStore.setApprovalStore(approvalStore());
         return jwtTokenStore;
@@ -72,9 +74,15 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
     }
 
     @Bean
-    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+    public JwtAccessTokenConverter jwtAccessTokenConverter() throws IOException {
         JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
-        accessTokenConverter.setSigningKey("cjs");
+        //org.springframework.core.io.Resource key = new ClassPathResource("smart_campus_auth_center_private_key.pem");
+        //accessTokenConverter.setSigningKey(IOUtils.toString(key.getInputStream()));
+        //accessTokenConverter.setVerifierKey();
+
+        org.springframework.core.io.Resource key = new ClassPathResource("smart_campus_auth_center.jks");
+        KeyPair keyPair = new KeyStoreKeyFactory(key, "ZDSoft.net@2018".toCharArray()).getKeyPair("smart_campus_auth_center");
+        accessTokenConverter.setKeyPair(keyPair);
         return accessTokenConverter;
     }
 }
